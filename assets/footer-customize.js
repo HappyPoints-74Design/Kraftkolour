33ca6ebf0565f7c5655404bf472e4b0939a48e79
Joseph <72374426+khanhtv990@users.noreply.github.com>
2024-06-07 14:20:49 +0700

    change font size menu footer

--- assets/footer-customize.js
+++ assets/footer-customize.js
@@ -3,10 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
 		el.addEventListener("click", (e) => {
 			let _this = e.target;
-
 			_this.parentNode.classList.toggle('active');
-
 		});
 
+		el.querySelector('.icon-and-text').addEventListener('click', (e) => {
+			let _target = e.target.parentNode.parentNode.parentNode.parentNode;
+			_target.classList.toggle('active');
+		})
+
 	})
+
 })
